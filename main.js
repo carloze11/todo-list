99671c23a950229c0e29b71336b5597a1bfa7ebb
Carlos Estrada <cestrada1109@gmail.com>
2022-09-03 20:18:48 -0500

    Add delete task button

--- main.js
+++ main.js
@@ -3,61 +3,77 @@ const createTask = (title, description, dueDate, priority) => {
     return {title, description, dueDate, priority}
 }
 
+const resetTasks = () => {
+    taskContainer.replaceChildren();
+    displayTasks();
+    completeTask();
+    deleteTask();
 
+}
 
 // Add tasks to list 
 // Going to have to add an iteration to this to make it work better
 // As of now, every input is taking in all values
 let tasks = []; 
-const btn = document.querySelector('button');
-btn.addEventListener('click', () => {
-    let title;
-    let description;
-    let dueDate;
-    let priority;
-    const formInputs = document.querySelectorAll('.input');
-    if (Array.from(formInputs).every(input => input.value !== '') === true){
-        for (let i = 0; i < formInputs.length; i++) {
-            if (formInputs[i].id === 'title'){
-                title = document.getElementById('title').value;
-            }
-            if (formInputs[i].id === 'description'){
-                description = document.getElementById('description').value;
-            }
-            if (formInputs[i].id === 'description'){
-                dueDate = document.getElementById('dueDate').value;
-            }
-            if (formInputs[i].id === 'description'){
-                priority = document.getElementById('priority').value;
-            }
-        }
-        tasks.push(createTask(title, description, dueDate, priority));
-        taskContainer.replaceChildren();
-        document.getElementById("fillTask").reset();
-        displayTasks();
-        completeTask();
-    }
-})
 
+const addTask = () => {
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', () => {
+        let title;
+        let description;
+        let dueDate;
+        let priority;
+        const formInputs = document.querySelectorAll('.input');
+        if (Array.from(formInputs).every(input => input.value !== '') === true){
+            for (let i = 0; i < formInputs.length; i++) {
+                if (formInputs[i].id === 'title'){
+                    title = document.getElementById('title').value;
+                }
+                if (formInputs[i].id === 'description'){
+                    description = document.getElementById('description').value;
+                }
+                if (formInputs[i].id === 'description'){
+                    dueDate = document.getElementById('dueDate').value;
+                }
+                if (formInputs[i].id === 'description'){
+                    priority = document.getElementById('priority').value;
+                }
+            }
+            tasks.push(createTask(title, description, dueDate, priority));
+            taskContainer.replaceChildren();
+            document.getElementById("fillTask").reset();
+            displayTasks();
+            deleteTask();
+            completeTask();
+        }
+    });
+};
+
+addTask();
 
 // Display tasks 
 const taskContainer = document.querySelector('.taskContainer');
 let div = document.createElement('div'); 
 
 const displayTasks = () => {
-    tasks.forEach( task => {
+    tasks.forEach( (task, i) => {
     const taskCard = div.cloneNode();
+    taskCard.setAttribute('id', `${i}`)
     const taskCompleteBtn = document.createElement('input');
     taskCompleteBtn.classList.add('task-btn')
     taskCompleteBtn.setAttribute('type', 'checkbox');
     taskCompleteBtn.setAttribute('id', 'checkbox');
-    taskCard.appendChild(taskCompleteBtn)
-    taskContainer.appendChild(taskCard);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('delete-btn');
+    deleteBtn.textContent = 'Delete'
+    taskCard.appendChild(taskCompleteBtn);
     for (const prop in task){
         let newDiv = div.cloneNode()
         taskCard.appendChild(newDiv)
         newDiv.textContent = task[prop]
-    }
+    };
+    taskCard.appendChild(deleteBtn);
+    taskContainer.appendChild(taskCard);
 })
 }
 
@@ -72,6 +88,18 @@ const completeTask = () => {
     })
 };
 
+
+// Delete task 
+const deleteTask = () => {
+    let deleteBtn = document.querySelectorAll('.delete-btn');
+    deleteBtn.forEach( (btn, i) => {
+        btn.addEventListener('click', () => {
+            console.log(btn.parentNode.id)
+            tasks.splice(btn.parentNode.id, 1);
+            resetTasks();
+        });
+    })
+}
 
 // Add new project 
 let projects = [];
